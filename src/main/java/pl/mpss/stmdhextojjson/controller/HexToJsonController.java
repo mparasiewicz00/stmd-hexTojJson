be748f097f5fd63cc1b7968fce76cbcf04d5c700
mateuszparasiewicz <m.parasiewicz00@gmail.com>
2024-07-15 20:31:58 +0200

    Little change in controller

--- src/main/java/pl/mpss/stmdhextojjson/controller/HexToJsonController.java
+++ src/main/java/pl/mpss/stmdhextojjson/controller/HexToJsonController.java
@@ -1,17 +1,17 @@
 package pl.mpss.stmdhextojjson.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pl.mpss.stmdhextojjson.service.HexToJsonService;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api")
 public class HexToJsonController {
 
-    @Autowired
-    private HexToJsonService hexToJsonService;
+    private final HexToJsonService hexToJsonService;
 
     @PutMapping("/data")
     public ResponseEntity<String> convertHexToJson(@RequestHeader("Authentication") String authHeader, @RequestBody byte[] data) {
